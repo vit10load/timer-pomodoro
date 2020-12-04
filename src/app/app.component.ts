3700a2c08c582a48d7aa2b57f5b015c83e40381f
vit580 <vitoro580@gmail.com>
2020-12-04 07:04:24 -0400

    inclusao de um novo audio alarme, alterações na lógica para verificação dos pomodoros realizados e por ultimo implementação do chrome notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { timer } from 'rxjs';
 
 
 @Component({
@@ -12,18 +11,18 @@ export class AppComponent {
 
   timeLeft: number = 1500;// in seconds
   secondsAux: number = this.timeLeft;
-  minutes: number = 25;
+  minutes: number = 24;
   minutesAux: number = this.minutes;
   counterIn60: number = 60;
   counter = 0;
   interval: any;
   pomodoros: number = 0;
   sum: number = 0;
-  audio = new Audio();
+  audio: any;
 
   timeLeftFive: number = 300;// in seconds
   secondsAuxFive: number = this.timeLeftFive
-  minutesFive: number = 5;
+  minutesFive: number = 4;
   minutesAuxFive: number = this.minutesFive
   counterFive: number = 0;
   counterIn60Five: number = 60;
@@ -31,7 +30,7 @@ export class AppComponent {
   sumFive: number = 0;
 
 
-
+ 
   /***
    * dependency injection for mat snack bar
    */
@@ -39,7 +38,18 @@ export class AppComponent {
 
   }
 
+  /***
+   * Init method for application
+   */
+  ngOnInit(): void {
+    this.audio = new Audio();
+    this.audio.src = '../../assets/alarme.wav';
+    this.audio.load();
+  }
 
+  /***
+   * Method for init timer in 25 minutes
+   */
   startTimer(): void {
     this.interval = setInterval(() => {
       this.timeLeft--;
@@ -50,18 +60,21 @@ export class AppComponent {
         this.counterIn60 = 60;
         this.counter = 0;
         if(this.sum == ((this.secondsAux/this.minutesAux) * this.minutesAux)){
-          this.audio.src = '../../assets/teste.ogg';
-          this.audio.load();
           this.audio.play();
           this.pomodoros++;
           this.sum = 0;
           this.counter = 0;
           this.timeLeft = this.secondsAux;
-          this.minutes = 25;
+          this.minutes = 24;
+          this.counterIn60 += 1;
+          if(this.pomodoros >= 4){
+            this.notiFyMe();
+            this._snack.open('Precisa-se fazer uma pausa de 10MIN!', ':)', {
+              duration: 5000,
+            });
+          }
           this.pauseTimer();
-          this._snack.open('Parabéns pomodoro realizado com sucesso!', ':)', {
-            duration: 4500,
-          });
+          
         }
     }
     this.counterIn60--;
@@ -73,6 +86,9 @@ export class AppComponent {
     clearInterval(this.interval);
   }
 
+  /**
+   * Method for init timer in 5 minutes
+   */
   startTimerForFiveMinutes(): void {
     this.intervalFive = setInterval(() => {
       this.timeLeftFive--;
@@ -83,13 +99,12 @@ export class AppComponent {
         this.counterIn60Five = 60;
         this.counterFive = 0;
         if(this.sumFive == ((this.secondsAuxFive/this.minutesAuxFive) * this.minutesAuxFive)){
-          this.audio.src = '../../assets/teste.ogg';
-          this.audio.load();
           this.audio.play();
           this.sumFive = 0;
           this.counterFive = 0;
           this.timeLeftFive = this.secondsAuxFive;
-          this.minutesFive = 5;
+          this.minutesFive = 4;
+          this.counterIn60Five += 1;
           this.pauseTimerForFiveMinutes();
           this._snack.open('Intervalo finalizado!', ':)', {
             duration: 4500,
@@ -100,8 +115,28 @@ export class AppComponent {
     },1000)
   }
 
+  /***
+   * Method for pause timer pomodoro
+   */
   pauseTimerForFiveMinutes(): void {
     clearInterval(this.intervalFive);
+  }
+
+  /***
+   * Method notification for timer 
+   */
+  notiFyMe(): void {
+
+    if (!("Notification" in window)) {
+    }else if(Notification.permission === "granted"){
+      let notiFy = new Notification("Recomenda-se uma pausa de 5 MIN.");
+    }else if(Notification.permission === "denied"){
+      Notification.requestPermission().then(function (permission){
+        if(permission === "granted"){
+          let notiFy = new Notification("Recomenda-se uma pausa de 5 MIN.");
+        }
+      });
+    }
   }
 
 }
